767c8a547f8beb7c49f546f900237317381d1b4c
sfrabby <fazlerabby6732@gmail.com>
2026-04-19 23:41:22 +0600

    Mixin fixed

--- mixin.dart
+++ mixin.dart
@@ -2,9 +2,17 @@ void main(){
 Person person = Person('fazle@gmail.com' , "1234");
 person.DisplayInfo();
 }
-class Person{
+class Person with Logger{
   String ? email,password;
+  Person(this.email, this.password);
   void DisplayInfo(){
     print("Email $email || Password : $password");
+  }
+}
+
+mixin Logger {
+  void log(String message){
+    print(message);
+
   }
 }
